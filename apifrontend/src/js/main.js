d903c8630a3b7cb42513670a3d2affee165d07f8
Steven <steventruong1995@yahoo.com>
2019-11-12 16:50:30 -0500

    links back to front

--- apifrontend/src/js/main.js
+++ apifrontend/src/js/main.js
@@ -1,6 +1,9 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import SideNav from "./components/SideNav"
+import Scientists from "./components/Scientists"
+import Subjects from "./components/Subjects"
+import Fields from "./components/Fields"
 
 export default () => {
     pageBuild();
@@ -10,6 +13,9 @@ function pageBuild(){
     footer();
     header();
     sideNav();
+    subjectNav();
+    fieldNav();
+    scientistNav();
 }
 
 function header(){
@@ -25,4 +31,35 @@ function sideNav(){
 function footer(){
     const footer = document.getElementById("footer");
     footer.innerHTML = Footer();
+}
+
+function subjectNav(){
+    const subjectButton = document.querySelector(".subjects");
+    
+    subjectButton.addEventListener("click", function(){
+        apiAction.getRequest("https://localhost:44330/api/subject", subjects => {
+            document.querySelector("#app").innerHTML = Subjects(subjects);
+        });
+    });
+}
+
+function fieldNav(){
+    const fieldButton = document.querySelector(".fields");
+    
+    fieldButton.addEventListener("click", function(){
+        apiAction.getRequest("https://localhost:44330/api/field", fields => {
+            document.querySelector("#app").innerHTML = Fields(fields);
+        });
+    });
+
+}
+
+function scientistNav(){
+    const scientistButton = document.querySelector(".scientists");
+    
+    scientistButton.addEventListener("click", function(){
+        apiAction.getRequest("https://localhost:44330/api/scientist", scientists => {
+            document.querySelector("#app").innerHTML = Scientists(scientists);
+        });
+    });
 }
